3e60572d8a9ac463bef93d0ce832d5d36507f994
Som Sahu <soms@zillowgroup.com>
2020-07-24 13:08:33 -0700

    edit response of the redirect to macth that of the original uri

---
+++ src/main/java/com/trivadis/kafka/schemaregistry/rest/extensions/interceptor/DynamicRegistrationForSubjectResource.java
@@ -0,0 +1,63 @@
+package com.trivadis.kafka.schemaregistry.rest.extensions.interceptor;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.confluent.kafka.schemaregistry.rest.resources.SubjectsResource;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.DynamicFeature;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.ext.WriterInterceptor;
+import javax.ws.rs.ext.WriterInterceptorContext;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class DynamicRegistrationForSubjectResource implements DynamicFeature {
+
+    public DynamicRegistrationForSubjectResource() { }
+
+    @Override
+    public void configure(ResourceInfo resourceInfo, FeatureContext context) {
+        if(resourceInfo.getResourceClass().equals(SubjectsResource.class) &&
+                resourceInfo.getResourceMethod().getName().contains("lookUpSchemaUnderSubject")) {
+            context.register(PostRedirectWriterInterceptor.class);
+        }
+    }
+}
+
+class PostRedirectWriterInterceptor implements WriterInterceptor {
+
+    public PostRedirectWriterInterceptor() { }
+
+    @Override
+    public void aroundWriteTo(WriterInterceptorContext context) throws IOException, WebApplicationException {
+        if(context.getProperty("post_filter_redirected") != null) {
+
+            OutputStream origOutputStream = context.getOutputStream();
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            context.setOutputStream(byteArrayOutputStream);
+
+            try {
+                context.proceed(); // let the MessageBodyWriter write the data
+                byte[] origResponseBytes = byteArrayOutputStream.toByteArray();
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+                Response origResponseObj = objectMapper.readValue(origResponseBytes, Response.class);
+                objectMapper = new ObjectMapper();
+                byte[] newResponseBytes = objectMapper.writeValueAsBytes(origResponseObj);
+                origOutputStream.write(newResponseBytes);
+            } finally {
+                context.setOutputStream(origOutputStream);
+            }
+        }
+        else {
+            context.proceed();
+        }
+    }
+}
+
+class Response {
+    public int id;
+}
